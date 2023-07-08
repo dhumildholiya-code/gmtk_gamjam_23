4cf4cdd3a07662694362306c362fd84afa1b27a3
Dhumil Dholiya <dhumildholiya97@gmail.com>
2023-07-09 01:59:58 +0530

    adde data reset to scriptable to reset data.

--- Assets/_Project/Scripts/SharkData.cs
+++ Assets/_Project/Scripts/SharkData.cs
@@ -6,7 +6,17 @@ namespace gmtk_gamejam
     public class SharkData : ScriptableObject
     {
         public int attackDamage;
-        public int attackRange;
+        public float attackRange;
         public int attackTarget;
+
+#if UNITY_EDITOR
+        [ContextMenu("Data Reset")]
+        public void DataReset()
+        {
+            attackDamage = 1;
+            attackRange = 6f;
+            attackTarget = 1;
+        }
+#endif
     }
 }
